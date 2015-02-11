de8a10f9f47df892e268fd43b1d1e0b298d62974
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-11 07:56:21 -0700

    don't redraw on every frame

--- src/breakin/render/PixiDebugRenderer.hx
+++ src/breakin/render/PixiDebugRenderer.hx
@@ -6,7 +6,6 @@ import edge.Entity;
 import edge.ISystem;
 import pixi.display.Stage;
 import pixi.primitives.Graphics;
-import pixi.renderers.IRenderer;
 import nape.shape.Shape;
 import nape.phys.*;
 using thx.core.Arrays;
@@ -14,43 +13,52 @@ import thx.color.HSL;
 
 class PixiDebugRenderer implements ISystem {
   var stage : Stage;
-  var g : Graphics;
-  var renderer : IRenderer;
-  var color : Int;
-  public function new(stage : Stage, renderer : IRenderer) {
+  var map : Map<Entity, Graphics>;
+  public function new(stage : Stage) {
     this.stage = stage;
-    this.renderer = renderer;
-    this.g = new Graphics();
+    map = new Map();
+  }
+
+  public function updateAdded(entity : Entity, data : { structure : Structure }) {
+    var g = new Graphics();
+    map.set(entity, g);
     stage.addChild(g);
-    color = HSL.create(200, 0.5, 0.5).toRGB();
-  }
-
-  public function before() {
-    g.clear();
+    var color = HSL.create(Math.random() * 360, 0.6, 0.5).toRGB();
     g.lineStyle(1, color, 0.75);
+    data.structure.shapes.pluck(render(g, _));
   }
 
+  public function updateRemoved(entity : Entity, data : { structure : Structure }) {
+    var g = map.get(entity);
+    map.remove(entity);
+    stage.removeChild(g);
+  }
+
+  var entity : Entity;
   public function update(structure : Structure) {
-    structure.shapes.pluck(render(_, structure.body));
+    var g = map.get(entity);
+    g.rotation = structure.body.rotation;
+    g.x = structure.body.position.x;
+    g.y = structure.body.position.y;
   }
 
-  function render(shape : Shape, body : Body) {
+  function render(g : Graphics, shape : Shape) {
     if(shape.isCircle()) {
       var c = shape.castCircle,
-          x = c.localCOM.x + body.position.x,
-          y = c.localCOM.y + body.position.y;
+          x = c.localCOM.x,
+          y = c.localCOM.y;
       g.drawCircle(x, y, c.radius);
       g.moveTo(x, y);
-      g.lineTo(x + Math.cos(body.rotation) * c.radius, y + Math.sin(body.rotation) * c.radius);
+      g.lineTo(x + c.radius, y);
     } else {
       var p = shape.castPolygon;
       g.moveTo(shape.localCOM.x,shape.localCOM.y);
       for(i in 0...p.worldVerts.length) {
         var px = p.localVerts.at(i);
-        g.lineTo(px.x + body.position.x, px.y + body.position.y);
+        g.lineTo(px.x, px.y);
       }
       var px = p.localVerts.at(0);
-      g.lineTo(px.x + body.position.x, px.y + body.position.y);
+      g.lineTo(px.x, px.y);
     }
   }
 }
