4cd32c138c95afbfc34be44a8c09d734626b639f
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-10 22:51:25 -0700

    more config options

--- src/Config.hx
+++ src/Config.hx
@@ -1,3 +1,8 @@
+import thx.color.HSL;
+
 class Config {
-  public static var wallSize = 20;
+  public static var backgroundColor = HSL.create(200, 0.5, 0.75);
+  public static var wallSize = 2;
+  public static var width = 800;
+  public static var height = 600;
 }
