ef8eb2849d62407b5395410e424fcfd6bff3875d
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-11 07:56:34 -0700

    changed background color

--- src/Config.hx
+++ src/Config.hx
@@ -1,7 +1,7 @@
 import thx.color.HSL;
 
 class Config {
-  public static var backgroundColor = HSL.create(200, 0.5, 0.75);
+  public static var backgroundColor = HSL.create(0, 0.0, 0.98);
   public static var wallSize = 2;
   public static var width = 800;
   public static var height = 600;
