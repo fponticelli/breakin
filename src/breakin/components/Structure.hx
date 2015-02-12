67e41f61e6a015352ee0d99f52ee403c5c060a60
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-12 00:07:31 -0700

    force structure to declare a material

--- src/breakin/components/Structure.hx
+++ src/breakin/components/Structure.hx
@@ -7,33 +7,32 @@ using thx.core.Nulls;
 class Structure {
   public var shapes : Array<Shape>;
   public var body : Body;
-  static var defaultMaterial = new Material(1,0.03,0.1,0.9,0.0001);
 
-  public function new(shapes : Array<Shape>, ?type : BodyType, ?material : Material) {
+  public function new(shapes : Array<Shape>, material : Material, ?type : BodyType) {
     this.shapes = shapes;
     this.body = new Body(type.or(BodyType.DYNAMIC));
     shapes.map(function(shape) {
       //body.shapes.add(shape);
       shape.body = body;
-      shape.material = material.or(defaultMaterial);
+      shape.material = material;
     });
   }
 
-  public static function ball(x : Float, y : Float, radius : Float, ?type : BodyType, ?material : Material) {
-    var body = new Structure([new Circle(radius)], type, material);
+  public static function ball(x : Float, y : Float, radius : Float, material : Material, ?type : BodyType) {
+    var body = new Structure([new Circle(radius)], material, type);
     body.body.position.setxy(x, y);
     return body;
   }
 
-  public static function rect(x : Float, y : Float, width : Float, height : Float, ?type : BodyType, ?material : Material)
-    return new Structure([new Polygon(Polygon.rect(x, y, width, height))], type, material);
+  public static function rect(x : Float, y : Float, width : Float, height : Float, material : Material, ?type : BodyType)
+    return new Structure([new Polygon(Polygon.rect(x, y, width, height))], material, type);
 
-  public static function perimeter(x : Float, y : Float, width : Float, height : Float, size : Float, ?material : Material) {
+  public static function perimeter(x : Float, y : Float, width : Float, height : Float, size : Float, material : Material) {
     return new Structure([
       new Polygon(Polygon.rect(x, y, width, size)),
       new Polygon(Polygon.rect(x + width - size, y + size, width, height - size * 2)),
       new Polygon(Polygon.rect(x, y + height - size, width, size)),
       new Polygon(Polygon.rect(x, size, size, height - size * 2))
-    ], BodyType.STATIC, material);
+    ], material, BodyType.STATIC);
   }
 }
