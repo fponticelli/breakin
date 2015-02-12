b3957e7d8a8a8e68a652438d2f66b29c58db609b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-02-12 00:07:15 -0700

    added spacebar and up to move the paddle up

--- src/breakin/interaction/KeyboardSystem.hx
+++ src/breakin/interaction/KeyboardSystem.hx
@@ -1,13 +1,18 @@
 package breakin.interaction;
 
+import breakin.components.*;
 import edge.cosystem.KeyboardInput;
 import edge.ISystem;
 
 class KeyboardSystem extends KeyboardInput implements ISystem {
-  function update() {
+  function update(direction : PaddleDirection) {
     for(k in keys) switch k.toString() {
       case "LEFT":
-        trace("GO LEFT");
+        direction.x -= 1;
+      case "RIGHT":
+        direction.x += 1;
+      case "UP", "SPACE":
+        direction.spring = true;
       case c:
         trace('not mapped: $c');
     }
